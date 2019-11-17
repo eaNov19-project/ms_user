a04dceb03e73310c4b82ad364a3561a7ac828844
islam ahmad <islam.ahmad@gmail.com>
2019-11-17 13:41:31 -0600

    converted home controller to rest controller

--- src/main/java/ea/sof/ms_user/controller/HomeController.java
+++ src/main/java/ea/sof/ms_user/controller/HomeController.java
@@ -1,11 +1,11 @@
 package ea.sof.ms_user.controller;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class HomeController {
     @GetMapping("/")
-    public ResponseEntity<?> home(){
+    public ResponseEntity<?> index(){
         return ResponseEntity.ok().build();
     }
 }
