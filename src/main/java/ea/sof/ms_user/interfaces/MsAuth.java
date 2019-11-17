420480d5ae4204bbc444294be71784463430e7c3
islam ahmad <islam.ahmad@gmail.com>
2019-11-16 20:03:44 -0600

    updates the feign client with new name
    removed the aplication port
    
    dont add it again to the code in the repo

--- src/main/java/ea/sof/ms_user/interfaces/MsAuth.java
+++ src/main/java/ea/sof/ms_user/interfaces/MsAuth.java
@@ -4,6 +4,6 @@ import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
 //@FeignClient(name="ms-auth", url = "http://localhost:8080/auth")
-@FeignClient(name="auth-ms", url = "${AUTHENTICATE_SERVICE}")
+@FeignClient(name="msAuth", url = "${AUTHENTICATE_SERVICE}")
 public interface MsAuth extends MsAuthShowcase {
 }
