009faa5fde03f61e95d3341f07c9c87a8bb278ce
rustem bayetov <rustem.bayetov@gmail.com>
2019-11-19 14:50:42 -0600

    added authenticate parameter

--- src/main/java/ea/sof/ms_user/interfaces/MsAuth.java
+++ src/main/java/ea/sof/ms_user/interfaces/MsAuth.java
@@ -4,6 +4,6 @@ import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
 //@FeignClient(name="ms-auth", url = "http://localhost:8080")
-@FeignClient(name="msAuth", url = "${AUTHENTICATE_SERVICE}")
+@FeignClient(name="msAuth", url = "${authenticate.service}")
 public interface MsAuth extends MsAuthShowcase {
 }
